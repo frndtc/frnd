bd6a3ead8c9fc9705ff4f45693dce05bc5a4b608
fernan <frndtimbalcmp@gmail.com>
2024-10-07 00:05:53 +0200

    Launch countdown
    - Added countdown
    - Changes in background video
    - Wider container
    - Changes in timer span shape

---
+++ script.js
@@ -0,0 +1,21 @@
+const countdownDate = new Date("December 31, 2024 23:59:59").getTime();
+
+const timer = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = String(days).padStart(2, '0');
+    document.getElementById("hours").innerText = String(hours).padStart(2, '0');
+    document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById("timer").innerText = "We're live!";
+    }
+}, 1000);
